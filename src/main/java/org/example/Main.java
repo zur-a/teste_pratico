a790c77c0e5f41ea5ff48eb19bd64df3f2739d0c
zura <jur.and.jr42@gmail.com>
2023-12-04 16:35:19 -0300

    Added method to setup list and remove an item

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import org.example.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Funcionario> funcionarios = generateList();
+        removeFuncionarioByName(funcionarios, "João");
+        for (Funcionario funcionario : funcionarios) {
+            System.out.println(funcionario);
+        }
+    }
+
+    private static ArrayList<Funcionario> generateList() {
+        ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
+        funcionarios.add(new Funcionario("Maria", LocalDate.of(2000, 10, 18), new BigDecimal("2009.44"), "Operador"));
+        funcionarios.add(new Funcionario("João", LocalDate.of(1990, 5, 12), new BigDecimal("2284.38"), "Operador"));
+        funcionarios.add(new Funcionario("Caio", LocalDate.of(1961, 5, 2), new BigDecimal("9836.14"), "Coordenador"));
+        funcionarios.add(new Funcionario("Miguel", LocalDate.of(1988, 10, 14), new BigDecimal("19119.88"), "Diretor"));
+        funcionarios.add(new Funcionario("Alice", LocalDate.of(1995, 1, 5), new BigDecimal("2234.68"), "Recepcionista"));
+        funcionarios.add(new Funcionario("Heitor", LocalDate.of(1999, 11, 19), new BigDecimal("1582.72"), "Operador"));
+        funcionarios.add(new Funcionario("Arthur", LocalDate.of(1993, 3, 31), new BigDecimal("4071.84"), "Contador"));
+        funcionarios.add(new Funcionario("Laura", LocalDate.of(1994, 7, 8), new BigDecimal("3017.45"), "Gerente"));
+        funcionarios.add(new Funcionario("Heloísa", LocalDate.of(2003, 5, 18), new BigDecimal("1606.85"), "Eletricista"));
+        funcionarios.add(new Funcionario("Helena", LocalDate.of(1996, 9, 2), new BigDecimal("2799.93"), "Gerente"));
+        return funcionarios;
+    }
+
+    //Remove um usuário da lista por nome
+    private static void removeFuncionarioByName(ArrayList<Funcionario> funcionarios, String nome) {
+        Iterator<Funcionario> iterator = funcionarios.iterator();
+        while (iterator.hasNext()) {
+            Funcionario funcionario = iterator.next();
+            if (funcionario.getNome().equals(nome)) {
+                iterator.remove();
+            }
+        }
+
     }
 }
--- src/main/java/org/example/model/Funcionario.java
+++ src/main/java/org/example/model/Funcionario.java
@@ -1,15 +1,15 @@
 package org.example.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Funcionario extends Pessoa {
     private BigDecimal salario;
     private String funcao;
 
-    public Funcionario() {
-    }
-
-    public Funcionario(BigDecimal salario, String funcao) {
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
         this.salario = salario;
         this.funcao = funcao;
     }
@@ -28,5 +28,15 @@ public class Funcionario extends Pessoa {
 
     public void setFuncao(String funcao) {
         this.funcao = funcao;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return "Nome: " + getNome() +
+                "\nData de Nascimento: " + getDataNascimento().format(formatter) +
+                "\nSalário: " + getSalario() +
+                "\nFunção: " + getFuncao() +
+                "\n---------------";
     }
 }
